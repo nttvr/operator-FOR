121dea9952ad9439d482c9ba20d2526ec3304e95
nttvr <166948404+nttvr@users.noreply.github.com>
2024-04-30 01:58:00 +0300

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,7 +1,8 @@
-list_ = ['BMW','MB','LADA','KIA','HONDA']
+list_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
 cars_count = 0
 for i in list_:
-    cars_count+=10
-    print('Я езжу на автомобиле марки',i)
-print(cars_count)
+    cars_count += 10
+    print('Я езжу на автомобиле марки', i)
+    print(cars_count)
+
 
